8ee1a4190d2b344e34a5b3ae9971cd5ba2f1be64
walter-cantori <walter.cantori@gmail.com>
2018-08-14 22:10:59 -0300

    add procfile

--- config/mail.js
+++ config/mail.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  host: process.env.MAIL_HOST_PROD,
-  port: process.env.MAIL_PORT_PROD,
-  user: process.env.MAIL_USER_PROD,
-  pass: process.env.MAIL_PASS_PROD,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  user: process.env.MAIL_USER,
+  pass: process.env.MAIL_PASS,
 
   templatePath: path.resolve('./resources/mail'),
 };
