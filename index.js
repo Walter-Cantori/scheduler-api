3da6d6d4ed71f38ee45f15ebf9ad8aa49215d228
walter-cantori <walter.cantori@gmail.com>
2018-08-14 22:32:30 -0300

    adjust index for prod

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
-const envPath = process.env.NODE_ENV
-  ? `.env.${process.env.NODE_ENV}`
-  : '.env';
-
-require('dotenv').config({ path: envPath });
+if (process.env.NODE_ENV !== 'production') {
+  const envPath = process.env.NODE_ENV
+    ? `.env.${process.env.NODE_ENV}`
+    : '.env';
+  require('dotenv').config({ path: envPath });
+}
 
 const { url, modelsPath } = require('./config/databse');
 
@@ -18,7 +19,7 @@ app.use(bodyParser.json());
 
 app.use('/api', require('./src/routes'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 module.exports = app;
